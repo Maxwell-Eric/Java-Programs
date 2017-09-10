1cd42cbc058c30fdb86cb811641f8119b6aad9c4
eek006 <eek006@hotmail.com>
2017-09-10 09:48:03 -0600

    Update StartWindow.java

--- Collision System/StartWindow.java
+++ Collision System/StartWindow.java
@@ -6,7 +6,6 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-//import javax.swing.SwingUtilities;
 
 public class StartWindow extends JFrame implements ActionListener {
 	
