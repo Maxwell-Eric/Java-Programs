f231e98d7f3af7f373268085952ccecc552a132a
eek006 <eek006@hotmail.com>
2018-05-09 21:50:02 -0600

    Update BBFrame.java

--- Collistion System with priority queue/BBFrame.java
+++ Collistion System with priority queue/BBFrame.java
@@ -1,13 +1,5 @@
 import java.awt.Color;
-//import java.awt.event.ActionEvent;
-//import java.awt.event.ActionListener;
-
-//import javax.swing.JButton;
-//import javax.swing.JComboBox;
 import javax.swing.JFrame;
-//import javax.swing.JLabel;
-
-//import edu.princeton.cs.algs4.StdDraw;
 
 public class BBFrame extends JFrame{
 	
@@ -21,7 +13,6 @@ public class BBFrame extends JFrame{
 	}
 
 	public static void main(String[] args){
-		
 		
 		BBFrame bbf = new BBFrame(500);
 		
